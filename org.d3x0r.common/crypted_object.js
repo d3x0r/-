283a99d678b61d3dd3a0c7499ec1996a1e04c747
d3x0r <d3x0r@users.noreply.github.com>
2016-10-12 23:00:45 -0700

    Improve test program; allow RNG Seeding to be optional and increase performance

--- org.d3x0r.common/crypted_object.js
+++ org.d3x0r.common/crypted_object.js
@@ -1,18 +1,12 @@
 "use strict";
 
-// there's also 'toJSON()' method which can be defined for an object.
-
+// there's also 'toJSON()' method which can be defined for an object, which is called before replacer
 
 var SRG = require( './salty_random_generator.js' );
-var RNG = SRG.SaltyRNG( );
-
 
 function ab2str(buf) {
 	//console.log( "what is buf?", buf, typeof( buf ) );
-	if( buf.byteLength & 1 )
-	  return String.fromCharCode.apply(null, new Uint8Array(buf));
-        else
-	  return String.fromCharCode.apply(null, new Uint16Array(buf));
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 
 function str2ab(str,skip) {
@@ -23,6 +17,74 @@ function str2ab(str,skip) {
     bufView[n++] = str.charCodeAt(i);
   }
   return buf;
+}
+
+function encodeString( data ) {
+			var n = 0;
+			var string = "";
+			for( n = 0; n < data.bytes_used; n++ ) {
+				var val = data[n];
+				if( !val )
+					string += "\\0";
+				else {
+					var out = String.fromCodePoint( val );
+					if( out === "\\" )
+						string += "\\/";
+					else if( out === "/" )
+						string += "\\|";
+					else
+						string += out;
+				}
+			}
+
+			
+			var array = new ArrayBuffer(string.length);
+			var escape = false;
+			var out = 0;
+			for( n = 0; n < string.length; n++ )
+				{ var val = string.codePointAt( n );
+					if( escape ) {
+						if( val === 48 )
+							val = 0;
+						escape = false;
+					}else {
+						if( val == 92 ) {
+							escape = true;
+							continue;
+						}
+					}
+					array[out++] = val;
+				 };
+				return array;
+}
+
+function decodeString( string, skip ){
+	var bytes = 0;
+			for( var n = skip; n < string.length; n++ ){
+				if( string[n] === '\\' ) {
+					continue;
+				} else bytes++;
+			}
+
+			var buffer = new ArrayBuffer(bytes);
+			bytes = 0;
+			for( var n = skip; n < string.length; n++ ){
+				if( string[n] === '\\' ) {
+					if( string[n+1] === '0' )
+						buffer[bytes++] = 0;
+					else if( string[n+1] === '/' )
+						buffer[bytes++] = '\\';
+					else if( string[n+1] === '|' )
+						buffer[bytes++] = '/';
+					else
+						throw new Error( "Invalid encoding" );
+					n++;
+					continue;
+				}
+				else
+					buffer[bytes++] = string.codePointAt( n );
+			}
+			return buffer;
 }
 
 function replacer(key,val) {
@@ -43,25 +105,25 @@ function replacer(key,val) {
 		   || (val instanceof ArrayBuffer && val.constructor == ArrayBuffer) ) {
     	//console.log( "buffer? ", val, val.buffer, type.constructor.name );
       if( type.constructor === Uint8Array )
-        return "U8:" + ab2str( val.buffer );
+        return "U8:" + encodeString( val.buffer );
       else if( type.constructor === Uint8ClampedArray )
-        return "U8C:" + ab2str( val.buffer );
+        return "U8C:" + encodeString( val.buffer );
       else if( type.constructor === Uint16Array )
-  	     return "U16:" + ab2str( val.buffer );
+  	     return "U16:" + encodeString( val.buffer );
       else if( type.constructor === Uint32Array )
-         return "U32:" + ab2str( val.buffer );
+         return "U32:" + encodeString( val.buffer );
       else if( type.constructor === Int8Array )
-         return "I8:" + ab2str( val.buffer );
+         return "I8:" + encodeString( val.buffer );
       else if( type.constructor === Int16Array )
-  	     return "I16:" + ab2str( val.buffer );
+  	     return "I16:" + encodeString( val.buffer );
       else if( type.constructor === Int32Array )
-         return "I32:" + ab2str( val.buffer );
+         return "I32:" + encodeString( val.buffer );
       else if( type.constructor === Float32Array )
-         return "F32:" + ab2str( val.buffer );
+         return "F32:" + encodeString( val.buffer );
       else if( type.constructor === Float64Array )
-  	     return "F64:" + ab2str( val.buffer );
+  	     return "F64:" + encodeString( val.buffer );
       else
-         return "AB:" + ab2str( val );
+         return "AB:" + encodeString( val );
     }
   }
   return val;
@@ -70,18 +132,19 @@ function replacer(key,val) {
 //exports.CryptObject
 //var
 exports.encryptObject = function( o, salt ) {
-    var RNG = new SRG.SaltyRNG( salt );
-    RNG.reset();
+    if( salt ) var RNG = new SRG.SaltyRNG( salt );
     let s = JSON.stringify( o, replacer );
-    let char_s = decodeURIComponent(escape(s));
-    let l = s.length;
+    //let char_s = decodeURIComponent(escape(s));
     //console.log( "output : ", char_s );
-    let output = new ArrayBuffer( l * 2 );
-    let outview = new Uint16Array( output );
-    for( let i = 0; i < l; i++ ) {
-        outview[i] = s.charCodeAt( i ) ^ RNG.getBits( 10 );
+    if( salt ) {
+        let l = s.length;
+        let output = new ArrayBuffer( l * 2 );
+        let outview = new Uint16Array( output );
+        for( let i = 0; i < l; i++ ) outview[i] = s.charCodeAt( i ) ^ RNG.getBits( 10 );
+        return String.fromCharCode.apply(null, outview);
+    }else {
+        return s;
     }
-    return String.fromCharCode.apply(null, outview);
 }
 
 function reviver( key, val ) {
@@ -93,34 +156,34 @@ function reviver( key, val ) {
     if( val.startsWith( "S:" ) ) {
         return val.slice( 2 );
     } else if( val.startsWith( "AB:" ) ) {
-    	var ab = str2ab( val, 3 );
+    	var ab = decodeString( val, 3 );
       return ab;
     } else if( val.startsWith( "U8:" ) ) {
-    	var ab = str2ab( val, 3 );
+    	var ab = decodeString( val, 3 );
       return new Uint8Array( ab );
     } else if( val.startsWith( "U8C:" ) ) {
-    	var ab = str2ab( val, 4 );
+    	var ab = decodeString( val, 4 );
       return new Uint8ClampedArray( ab );
     } else if( val.startsWith( "I8:" ) ) {
-    	var ab = str2ab( val, 3 );
+    	var ab = decodeString( val, 3 );
       return new Int8Array( ab );
     } else if( val.startsWith( "U16:" ) ) {
-    	var ab = str2ab( val, 4 );
+    	var ab = decodeString( val, 4 );
       return new Uint16Array( ab );
     } else if( val.startsWith( "I16:" ) ) {
-    	var ab = str2ab( val, 4 );
+    	var ab = decodeString( val, 4 );
       return new Int16Array( ab );
     } else if( val.startsWith( "U32:" ) ) {
-    	var ab = str2ab( val, 4 );
+    	var ab = decodeString( val, 4 );
       return new Uint32Array( ab );
     } else if( val.startsWith( "I32:" ) ) {
-    	var ab = str2ab( val, 4 );
+    	var ab = decodeString( val, 4 );
       return new Int32Array( ab );
     } else if( val.startsWith( "F32:" ) ) {
-    	var ab = str2ab( val, 4 );
+    	var ab = decodeString( val, 4 );
       return new Float32Array( ab );
     } else if( val.startsWith( "F64:" ) ) {
-    	var ab = str2ab( val, 4 );
+    	var ab = decodeString( val, 4 );
       return new Float64Array( ab );
     }
   }
@@ -128,7 +191,7 @@ function reviver( key, val ) {
 }
 
 exports.decryptObject = function( str, salt ) {
-    var RNG = new SRG.SaltyRNG( salt );
+    if( salt ) var RNG = new SRG.SaltyRNG( salt );
     let l = str.length;
     var buf = new ArrayBuffer(l*2); // 2 bytes for each char
     var bufView = new Uint16Array(buf);
@@ -136,14 +199,16 @@ exports.decryptObject = function( str, salt ) {
         bufView[i] = str.charCodeAt(i);
     }
 
-    let output = new ArrayBuffer( (l) * 2 );
-    let outview = new Uint16Array( output );
-    for( let i = 0; i < l; i++ ) {
-        outview[i] = bufView[ i ] ^ RNG.getBits( 10 );
-    }
-
-    let s = String.fromCharCode.apply(null, outview );
-    //console.log( "decode ", s );
+    let s;
+    if( salt ) { 
+        let output = new ArrayBuffer( (l) * 2 );
+        let outview = new Uint16Array( output );
+        for( let i = 0; i < l; i++ ) { outview[i] = bufView[ i ] ^ RNG.getBits( 10 ); }
+        s = String.fromCharCode.apply(null, outview );
+        //console.log( "decode ", s );
+    }else
+        s = String.fromCharCode.apply(null, bufView );
     let obj = JSON.parse( s, reviver );
+
     return obj;
 }
--- org.d3x0r.common/test.js
+++ org.d3x0r.common/test.js
@@ -1,32 +1,76 @@
-"use strict";
-var text = require( "./text.js" );
-var command = text.Parse( "/create mary had a little lamb, it's fleece was white as snow" );
-//var command = text.Parse( "mary had a little lamb, it's fleece was white as snow" );
 
-//console.log( "command : ", command)
-if( command ) {
-		//if( recording ) {
-		//}
-		//else
-		 if( command.text === "/" )
-		{
-			//console.log( "command ", String( command ) );
-		        console.log( command.next.text );
-			var cmd = command.next.text;
-			if( cmd === 'create' ) {
-				// do something with the rest of the words...
-				// would pass a wordref to this point?
-				//command.next.next.forEach( (word)=>{ console.log( `word:'${word.text}'` )})
-			}
-			if( cmd === 'Mary' ) {
-				console.log( "Marry ");
-			}
-			if( cmd === 'mary' ) {
-				console.log( "Marry ");
-			}
-		}
-		else
-		{
-			console.log( "send_data '", String(command), "'" );
-		}
+var SaltyRNG = require( "../org.d3x0r.common/salty_random_generator.js" );
+var cryptObj = require( "./crypted_object" );
+
+//console.log( Object.keys( module ) );
+//console.log( module );
+//console.log( Object.keys( process ) );
+//console.log( Object.keys( Buffer ) );
+
+//var global = /*Components.*/ utils.getGlobalForObject(cryptObj);
+
+console.log( "..." );
+
+var RNG = SaltyRNG.SaltyRNG( );
+var n;
+//for( n = 0; n < 8; n++ )
+//  console.log( RNG.getBits( 8 ).toString( 16 ) );
+
+function dumpObj( o ) {
+    var keys = Object.keys( o );
+    console.log( "keys : " + keys);
+    for( var i = 0; i < keys.length; i++ )
+        console.log( "%s %s", keys[i], o[keys[i]] )
 }
+
+var random = new Date().valueOf();
+function keyGen( key ) {
+	key.push( "asdf" );
+        key.push( random) ;
+}
+
+var tmp = { from: "user@domain.com", date: new Date(), msg : "message goes here"
+		, group_id:1234
+		, buf:new ArrayBuffer(15)
+		, array:[10,34,23]
+                , typdArray : new Uint32Array( 10 ) };
+
+dumpObj( tmp );
+var s = cryptObj.encryptObject( tmp, keyGen );
+console.log( "encrypted s : %s", s );
+var o = cryptObj.decryptObject( s, keyGen );
+dumpObj( o );
+
+
+var start = (new Date()).valueOf();
+for( var n = 0; n < 10000; n++ ) {
+	s = cryptObj.encryptObject( tmp );
+}
+console.log( "encrypted s : %s", s );
+var end = (new Date()).valueOf();
+console.log( "del : %d %d", ( end-start ), ( ( end-start ) / 1000 ) / 10000  );
+
+var start = (new Date()).valueOf();
+var tmp2;
+for( var n = 0; n < 10000; n++ )
+ 	tmp2 = cryptObj.decryptObject( s );
+
+console.log( "And done with", tmp2 );
+var end = (new Date()).valueOf();
+console.log( "del : " + ( end-start ) );
+
+
+/*
+var tmpRNG = RNG.getBuffer( 4096 );
+var val = "";
+for( n = 0; n < 4096 / 32; n++ )
+    val = val + tmpRNG[n].toString(16);
+    console.log( "%s", val );
+//console.log( RNG.getBuffer( 4096 ).toString( 16 ) );
+console.log( RNG.getBits( 32 ).toString( 16 ) );
+console.log( RNG.getBits( 32 ).toString( 16 ) );
+console.log( RNG.getBits( 32 ).toString( 16 ) );
+console.log( RNG.getBits( 32 ).toString( 16 ) );
+
+//RNG.getBits( 3 );
+*/

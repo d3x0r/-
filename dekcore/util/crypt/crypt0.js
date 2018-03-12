f3eb87feb7a29a5c92ca51055e7e72845643472b
d3x0r <d3x0r@no-reply.github.com>
2018-03-11 22:15:45 -0700

    Fix abstraction a little... output less expanded form

--- dekcore/util/crypt/crypt0.js
+++ dekcore/util/crypt/crypt0.js
@@ -23,6 +23,8 @@ var cpa = "['\\x63\\x6f\\x64\\x65\\x50\\x6f\\x69\\x6e\\x74\\x41\\x74']"
 var fcp = "['\\x66\\x72\\x6f\\x6d\\x43\\x6f\\x64\\x65\\x50\\x6f\\x69\\x6e\\x74']"
 var map = "['\\x6d\\x61\\x70']"
 var join = "['\\x6a\\x6f\\x69\\x6e']"
+var _eval = "'\\x65\\x76\\x61\\x6c'"
+var _String = "'\\x53\\x74\\x72\\x69\\x6e\\x67'";
 
-//vol.write( oname, `π=${JSON.stringify(out)};eval(((θ)=>{const π=[...${JSON.stringify(ekey)}].map((π)=>π.codePointAt(0));const ϕ=π.length;return [...θ].map((ϵ,Λ)=>String.fromCodePoint(ϵ.codePointAt(0)^π[Λ%ϕ])).join('');})(π))`);
-vol.write( oname, `π=${JSON.stringify(out)};eval(((θ,ϕ)=>((π=[...${JSON.stringify(ekey)}]${map}((π)=>π${cpa}(0))),(ϕ=π${length}),[...θ]${map}((ϵ,Λ)=>String${fcp}(ϵ${cpa}(0)^π[Λ%ϕ]))${join}('')))(π))`);
+vol.write( oname+"2", `π=${JSON.stringify(out)};eval(((θ)=>{const π=[...${JSON.stringify(ekey)}].map((π)=>π.codePointAt(0));const ϕ=π.length;return [...θ].map((ϵ,Λ)=>String.fromCodePoint(ϵ.codePointAt(0)^π[Λ%ϕ])).join('');})(π))`);
+vol.write( oname, `π=${JSON.stringify(out)};this[${_eval}](((θ,ϕ)=>((π=[...${JSON.stringify(ekey)}]${map}((π)=>π${cpa}(0))),(ϕ=π${length}),[...θ]${map}((ϵ,Λ)=>this[${_String}]${fcp}(ϵ${cpa}(0)^π[Λ%ϕ]))${join}('')))(π))`);

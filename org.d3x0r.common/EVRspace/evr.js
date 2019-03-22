4172df3cffb3d36f32e2cb31d8d6bb5a75ba33d2
d3x0r <d3x0r@no-reply.github.com>
2019-03-22 14:51:16 -0700

    Some updates to EVR

--- org.d3x0r.common/EVRspace/evr.js
+++ org.d3x0r.common/EVRspace/evr.js
@@ -20,7 +20,7 @@ var rng = require('../salty_random_generator.js').SaltyRNG((salt) => { salt.leng
 const charset = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g"
 	, "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x"
 	, "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"
-	, "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "-", "_"];
+	, "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "$", "_"];
 
 function makeKey() {
 	var res = "";

e578adee478db84d400226a0e9e08ea037564c99
d3x0r <d3x0r@no-reply.github.com>
2019-11-11 05:26:18 -0800

    Add startup script to add web interface launcher

---
+++ dekcore/startupWeb.js
@@ -0,0 +1,12 @@
+
+
+function buildPiping(){
+	// the server part of this doesn't actually have IO
+	
+	require( './command_stream_filter/webConsole.js' ).Filter();
+}
+
+buildPiping();
+
+console.log( "Hello from startup Web.js" );//, Object.keys(this) );
+

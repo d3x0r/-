19722d6016df75e902528cc2fb47b98ee20c02c3
d3x0r <d3x0r@no-reply.github.com>
2020-12-21 01:13:04 -0800

    Add service worker background resource loader; and local work agent

---
+++ dekcore/command_stream_filter/ui/common/mk.bat
@@ -0,0 +1,3 @@
+
+:rollup --format=cjs --file=swbundle.js --no-esModule --  sw.js
+node "c:\users\d3x0r\appdata\roaming\npm\node_modules\rollup\dist\bin\rollup"  --format=cjs --file=../swbundle.js --no-esModule --  sw.js

29f9820668e37b7175e0ce29c43c7bd23aeea5ec
d3x0r <d3x0r@no-reply.github.com>
2021-02-09 00:49:54 -0800

    Fix Z base

--- a-star/perlin-sphere.js
+++ a-star/perlin-sphere.js
@@ -2,7 +2,6 @@ import {SaltyRNG} from "./salty_random_generator.js"
 
 const CUBE_ELEMENT_SIZE = 16
 
-
 function noise( s, opts ) {
 	function NoiseGeneration(n,s) {
 		return { 
@@ -75,6 +74,7 @@ function noise( s, opts ) {
 		var patch = last_used;
 		
 	}
+		var counter = 0;
 	function heatCache( patch ) {
 		var p1 = most_used;
 		var _p1 = null;
@@ -94,7 +94,6 @@ function noise( s, opts ) {
 		}
 		patch.when = Date.now();
 		if( cacheLen > 500 ) {
-			var counter = 0;
 			for( p1 = most_used; p1; (_p1 = p1),p1 = p1.next ) {
 				counter++;
 				if( counter === 400 ) {
@@ -117,7 +116,6 @@ function noise( s, opts ) {
 		}
 		if( counter++ == 1000 ) {
 			counter = 0;
-		console.log( "length?", cacheLen, patch );
 		}
 		//patch.next = null;
 	}
@@ -158,290 +156,6 @@ function noise( s, opts ) {
 			noiseGen[g].oz = oz||0;
 		}, 
 
-		get(x,y,z,s) {
-			z = z || 0;
-			var side = (x > y);
-			var ox = x;
-			var oy = y;
-			var oz = z;
-
-			var minX = 0;
-			var maxX = opts.patchSize;
-			if( s >= 6 && s <= 6 ) {
-				if( ox > oy ) {		
-					x =  (ox+ ox-oy) * (opts.patchSize)/(ox+1);
-					y = ox-opts.patchSize;
-				} else {
-					x =  (x) * (opts.patchSize)/(y+1);
-					y = ( y - opts.patchSize );
-				} 
-			}
-			if( s >= 7 && s <= 7 ) {
-				if( ox > oy ) {			
-					x = (ox*3+ox-oy) * (opts.patchSize)/(ox+1);
-					y = ox-opts.patchSize;
-
-				} else {
-					x =  (y*2+(x+2)) * (opts.patchSize)/(y+1);
-					y = ( y - opts.patchSize );
-				} 
-			}
-			if( s >= 8 && s <= 8 ) {
-				if( ox > oy ) {			
-					x =  (ox*5+ox-oy) * (opts.patchSize)/(ox+1);
-					y = ox-opts.patchSize;
-				} else {
-					x =  ((y*4+(x+4))) * (opts.patchSize)/(y+1);
-					y = ( y - opts.patchSize );
-				} 
-			}
-
-
-			if( s >= 9 && s <= 9 ) {
-				oy = y = opts.patchSize - y;
-				if( ox > oy ) {		
-					x =  (ox+ ox-oy) * (opts.patchSize)/(ox+1);
-				        y = ox-opts.patchSize;
-				} else {
-					x =  (x) * (opts.patchSize)/(y+1);
-					y = ( y - opts.patchSize );
-				} 
-				y = opts.patchSize-y;
-			}
-			if( s >= 10 && s <= 10 ) {
-				oy = y = opts.patchSize - y;
-				if( ox > oy ) {			
-					x = (ox*3+ox-oy) * (opts.patchSize)/(ox+1);
-				        y = ox-opts.patchSize;
-
-				} else {
-					x =  (y*2+(x+2)) * (opts.patchSize)/(y+1);
-					y = ( y - opts.patchSize );
-				} 
-				y = opts.patchSize-y;
-			}
-			if( s >= 11 && s <= 11 ) {
-				oy = y = opts.patchSize - y;
-				if( ox > oy ) {			
-					x =  (ox*5+ox-oy) * (opts.patchSize)/(ox+1);
-					y = ox-opts.patchSize;
-				} else {
-					x =  ((y*4+(x+4))) * (opts.patchSize)/(y+1);
-					y = ( y - opts.patchSize );
-				} 
-				y = opts.patchSize-y;
-			}
-			
-			minX = 0; maxX = opts.patchSize*6;
-			//return x/(6*opts.patchSize);
-			//return 0.5+(y)/(4*opts.patchSize);
-
-			var dolog = false;
-
-//	        	x += opts.patchSize;        
-	        	y += 3*opts.patchSize;        
-
-			var _x;
-			// Y will be 0 at the same time this changes...  which will update all anyway
-			for( var n = 0; n < noiseGen.length; n++ ) {
-				var gen = noiseGen[n];
-
-				var offset = noiseGen.length-n;//opts.base % CUBE_ELEMENT_SIZE;
-				var mod = Infinity;
-				var nx;
-				var ny;
-				var nz;
-				var npx, npy, npz;
-				switch(s) {
-				case 0:
-				case 1:
-				case 2:
-				case 3:
-				case 4:
-					//break; // no remaps needed.
-				case 5:
-				//case 6:
-				//case 7:
-					nx = Math.floor(( (x-gen.ox+s*opts.patchSize) / gen.pitch ) )*gen.pitch;
-					ny = Math.floor(( (y-gen.oy) / gen.pitch ) )*gen.pitch;
-					nz = Math.floor(( (z-gen.oz) / gen.pitch ) )*gen.pitch;
-					npx = nx + gen.pitch;
-					npy = ny + gen.pitch;
-					npz = nz+ gen.pitch;
-					if( nx < gen.ox ) {
-						nx += 6*opts.patchSize;
-						npx = nx + gen.pitch;
-					}
-					if( npx >= (gen.ox + 6*opts.patchSize ) )
-						npx -= 6*opts.patchSize;
-					//mod = 6*opts.patchSize;
-					//nx += s*opts.patchSize;
-					gen.cx = ((x - gen.ox + s * opts.patchSize)/gen.pitch) % 1;
-					while( gen.cx < 0 ) { gen.cx += 1; /*nx -=1*/ }
-					gen.cy = ((y - gen.oy)/gen.pitch) % 1;
-					while( gen.cy < 0 ) { gen.cy += 1; /*ny -= 1*/ }
-					gen.cz = ((z - gen.oz)/gen.pitch) % 1;
-					while( gen.cz < 0 ) gen.cz += 1;
-					break;
-				case 6:
-				case 7:
-				case 8:
-
-				case 9:
-				case 10:
-				case 11:
-				//case 11:
-	//	if( dolog )                     
-						nx = Math.floor(( (x-gen.ox) / gen.pitch ) )*gen.pitch;
-						ny = Math.floor(( (y-gen.oy) / gen.pitch ) )*gen.pitch;
-						nz = Math.floor(( (z-gen.oz) / gen.pitch ) )*gen.pitch;
-						if( nx < minX ) {
-							npx = nx;
-							return 0.9;
-						} 
-						else if( /*s == 8 && */ (nx + gen.pitch )>  maxX ) {
-							
-							npx = Math.floor(( (minX) / gen.pitch ) )*gen.pitch;
-							npy = ny + gen.pitch;
-							return 0.9;
-							npz = nz + gen.pitch;
-						} else {
-							npx = nx + gen.pitch;
-							npy = ny + gen.pitch;
-							npz = nz + gen.pitch;
-						}
-
-						if( /*s == 8 && */ (npx)>  maxX ) {
-							npx = nx;
-							//npx = Math.floor(( (minX) / gen.pitch ) )*gen.pitch;
-						}
-
-					if( npx >= (gen.ox + 6*opts.patchSize ) ) {
-						//console.log( "Wrap success?" );
-						npx -= 6*opts.patchSize;
-					}
-
-//				if ( ( s == 6 &&  ox == 0 )  || ( s == 8  && oy == 0 ) )
-//					console.log( "s:", s, "p:", gen.pitch, "mm:", minX, maxX, "result on xy:", ox, oy, "xy:", x, y, "nxy:", nx, ny, "npxy:", npx, npy );
-
-						//if( s == 8 && n == 0 )                     
-						//	console.log( "so...", ox, oy, "xy:", x, y, "nxy:", nx, ny, npx, npy );
-		        
-						gen.cx = ((x - gen.ox)/gen.pitch) % 1;
-						while( gen.cx < 0 ) {
-						return 0.9;
-							 gen.cx += 1; /*nx -=1*/ 
-						}
-						gen.cy = ((y - gen.oy)/gen.pitch) % 1;
-						while( gen.cy < 0 ) { gen.cy += 1; /*ny -= 1*/ }
-						gen.cz = ((z - gen.oz)/gen.pitch) % 1;
-						while( gen.cz < 0 ) gen.cz += 1;
-
-					break;
-				}
-				if( ny != gen.ny  ) { 
-					gen.dirty = true; 
-					//console.log( "using", offset + (ny) * opts.patchSize + (nx ),offset + ( (ny) %opts.patchSize ) * opts.patchSize + (nx) );
-				}
-				if( nx != gen.nx ) { 
-					gen.dirty = true; 
-				}
-				if( nz != gen.nz ) { 
-					gen.dirty = true; 
-				}
-				//if( ( s == 6 && gen.steps == 4) ||
-				//    ( s == 0 && gen.steps == 4) )
-				//	console.log( "COORDS:", s, "XY:", x, y, "NXY:", nx, ny, "NPXY:",npx, npy );
-				if( nx == npx ) {
-					gen.cx = gen.cy = gen.cz = 0;
-					gen.dx1 = gen.dx2 = gen.dx3 = gen.dx4 = 0;
-					gen.corn = [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ]; 
-				} else 
-
-				if( gen.dirty ) {
-					gen.dirty = false;
-					//console.log( "noise is...", n, x, y, z, nx, ny, nz, gen.cx, gen.cy, gen.cz );
-					gen.nx = nx; gen.ny =ny; gen.nz = nz;
-
-					var noise1;
-					var noise2;
-					var noise3;
-					var noise4;
-		
-					//var noise5;
-					//var noise6;
-					//var noise7;
-					//var noise8;
-						noise1 = getRandom( nx   , ny          , nz ).arr;
-						noise2 = getRandom( (npx), ny          , nz ).arr;
-						noise3 = getRandom( nx   , (npy), nz ).arr;
-						noise4 = getRandom( (npx), (npy), nz ).arr;
-		                        
-					//	noise5 = getRandom( nx   , ny          , (nz+gen.pitch) ).arr;
-					//	noise6 = getRandom( (npx), ny          , (nz+gen.pitch) ).arr;
-					//	noise7 = getRandom( nx   , npy, (nz+gen.pitch) ).arr;
-					//	noise8 = getRandom( (npx), npy, (nz+gen.pitch) ).arr;
-
-					gen.ix = nx % CUBE_ELEMENT_SIZE;
-					while( gen.ix < 0 ) gen.ix += CUBE_ELEMENT_SIZE;
-					gen.jx = (gen.ix+gen.pitch)%CUBE_ELEMENT_SIZE;
-
-					gen.iy = ny % CUBE_ELEMENT_SIZE;
-					while( gen.iy < 0 ) gen.iy += CUBE_ELEMENT_SIZE;
-					gen.jy = (gen.iy+gen.pitch)%CUBE_ELEMENT_SIZE;
-
-					//gen.iz = nz % CUBE_ELEMENT_SIZE;
-					//while( gen.iz < 0 ) gen.iz += CUBE_ELEMENT_SIZE;
-					//gen.jz = (gen.iz+gen.pitch)%CUBE_ELEMENT_SIZE;
-				
-					gen.corn[0] = noise1[  (gen.iy) * CUBE_ELEMENT_SIZE + gen.ix ];
-					gen.corn[1] = noise2[  (gen.iy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
-
-					gen.corn[2] = noise3[  (gen.jy) * CUBE_ELEMENT_SIZE + gen.ix ];
-					gen.corn[3] = noise4[  (gen.jy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
-
-
-					//gen.corn[4] = noise5[ (gen.jz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) + gen.iy * CUBE_ELEMENT_SIZE + gen.ix ];
-					//gen.corn[5] = noise6[ (gen.jz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) + (gen.iy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
-
-					//gen.corn[6] = noise7[ (gen.jz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) + (gen.jy) * CUBE_ELEMENT_SIZE + gen.ix ];
-					//gen.corn[7] = noise8[ (gen.jz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) + (gen.jy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
-
-					gen.dx1 = gen.corn[1] - gen.corn[0];
-					gen.dx2 = gen.corn[3] - gen.corn[2];
-
-					//gen.dx3 = gen.corn[5] - gen.corn[4];
-					//gen.dx4 = gen.corn[7] - gen.corn[6];
-
-				}
-			}
-		
-		
-			var tot = 0;
-			for( var n = 0; n < noiseGen.length; n++ ) {
-				var gen = noiseGen[n];
-				// ((((c1)*(max-(d))) + ((c2)*(d)))/max)				
-				//console.log( "gen.cx:", gen.cx );
-				var tx = (1-Math.cos( gen.cx *Math.PI) )/2;
-				var ty =  (1-Math.cos( gen.cy *Math.PI) )/2;
-
-				var value1 = gen.dx1 * ( tx ) + gen.corn[0];
-				var value2 = gen.dx2 * ( tx ) + gen.corn[2];
-		
-				var dy1 = value2 - value1; // /1
-				var value12 = value1 + ty * dy1;
-
-				tot += (value12 * gen.scalar) ;				
-			}
-
-			tot /= maxtot;
-			//console.log( "value: ", tot );
-			//outNoise.push( noiseGen[3].corn[0] );
-			//	if( maxVal < tot ) maxVal = tot;
-				//if( minVal > tot ) minVal = tot;
-			
-			return tot;
-		},
 
 		get2(x,y,z,s) {
 			z = z || 0;
@@ -450,19 +164,6 @@ function noise( s, opts ) {
 			var oy = y;
 			var oz = z;
 
-			var minX = 0;
-			var maxX = opts.patchSize;
-
-			minX = 0; maxX = opts.patchSize*6;
-			//return x/(6*opts.patchSize);
-			//return 0.5+(y)/(4*opts.patchSize);
-
-			var dolog = false;
-
-//	        	x += opts.patchSize;        
-	        	y += 3*opts.patchSize;        
-
-			var _x;
 			// Y will be 0 at the same time this changes...  which will update all anyway
 			for( var n = 0; n < noiseGen.length; n++ ) {
 				var gen = noiseGen[n];
@@ -476,26 +177,23 @@ function noise( s, opts ) {
 				//const oldOffset = s*opts.patchSize;
 				const oldOffset = 0;
 
-					nx = Math.floor(( (x-gen.ox+oldOffset) / gen.pitch ) )*gen.pitch;
+					nx = Math.floor(( (x-gen.ox) / gen.pitch ) )*gen.pitch;
 					ny = Math.floor(( (y-gen.oy) / gen.pitch ) )*gen.pitch;
 					nz = Math.floor(( (z-gen.oz) / gen.pitch ) )*gen.pitch;
 					npx = nx + gen.pitch;
 					npy = ny + gen.pitch;
 					npz = nz + gen.pitch;
-					if( nx < gen.ox ) {
-						nx += 6*opts.patchSize;
-						npx = nx + gen.pitch;
-					}
-					if( npx >= (gen.ox + 6*opts.patchSize ) )
-						npx -= 6*opts.patchSize;
+
 					//mod = 6*opts.patchSize;
 					//nx += s*opts.patchSize;
-					gen.cx = ((x - gen.ox + oldOffset)/gen.pitch) % 1;
+					gen.cx = ((x - gen.ox)/gen.pitch) % 1;
 					while( gen.cx < 0 ) { gen.cx += 1; /*nx -=1*/ }
+
 					gen.cy = ((y - gen.oy)/gen.pitch) % 1;
 					while( gen.cy < 0 ) { gen.cy += 1; /*ny -= 1*/ }
+
 					gen.cz = ((z - gen.oz)/gen.pitch) % 1;
-					while( gen.cz < 0 ) gen.cz += 1;
+					while( gen.cz < 0 ) { gen.cz += 1; }
 
 				if( ny != gen.ny  ) { 
 					gen.dirty = true; 
@@ -509,12 +207,6 @@ function noise( s, opts ) {
 				//if( ( s == 6 && gen.steps == 4) ||
 				//    ( s == 0 && gen.steps == 4) )
 				//	console.log( "COORDS:", s, "XY:", x, y, "NXY:", nx, ny, "NPXY:",npx, npy );
-				if( nx == npx ) {
-					gen.cx = gen.cy = gen.cz = 0;
-					gen.dx1 = gen.dx2 = gen.dx3 = gen.dx4 = 0;
-					gen.corn = [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ]; 
-				} else 
-
 				if( gen.dirty ) {
 					gen.dirty = false;
 					//console.log( "noise is...", n, x, y, z, nx, ny, nz, gen.cx, gen.cy, gen.cz );
@@ -529,13 +221,13 @@ function noise( s, opts ) {
 					var noise6;
 					var noise7;
 					var noise8;
-						noise1 = getRandom( nx   , ny          , nz ).arr;
-						noise2 = getRandom( (npx), ny          , nz ).arr;
+						noise1 = getRandom( nx   , ny   , nz ).arr;
+						noise2 = getRandom( (npx), ny   , nz ).arr;
 						noise3 = getRandom( nx   , (npy), nz ).arr;
 						noise4 = getRandom( (npx), (npy), nz ).arr;
 		                        
-						noise5 = getRandom( nx   , ny          , (npz) ).arr;
-						noise6 = getRandom( (npx), ny          , (npz) ).arr;
+						noise5 = getRandom( nx   , ny , (npz) ).arr;
+						noise6 = getRandom( (npx), ny , (npz) ).arr;
 						noise7 = getRandom( nx   , npy, (npz) ).arr;
 						noise8 = getRandom( (npx), npy, (npz) ).arr;
 
@@ -551,11 +243,11 @@ function noise( s, opts ) {
 					while( gen.iz < 0 ) gen.iz += CUBE_ELEMENT_SIZE;
 					gen.jz = (gen.iz+gen.pitch)%CUBE_ELEMENT_SIZE;
 				
-					gen.corn[0] = noise1[  (gen.iy) * CUBE_ELEMENT_SIZE + gen.ix ];
-					gen.corn[1] = noise2[  (gen.iy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
+					gen.corn[0] = noise1[ (gen.iz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) +  (gen.iy) * CUBE_ELEMENT_SIZE + gen.ix ];
+					gen.corn[1] = noise2[ (gen.iz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) +  (gen.iy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
 
-					gen.corn[2] = noise3[  (gen.jy) * CUBE_ELEMENT_SIZE + gen.ix ];
-					gen.corn[3] = noise4[  (gen.jy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
+					gen.corn[2] = noise3[ (gen.iz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) +  (gen.jy) * CUBE_ELEMENT_SIZE + gen.ix ];
+					gen.corn[3] = noise4[ (gen.iz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) +  (gen.jy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
 
 					gen.corn[4] = noise5[ (gen.jz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) + gen.iy * CUBE_ELEMENT_SIZE + gen.ix ];
 					gen.corn[5] = noise6[ (gen.jz) * (CUBE_ELEMENT_SIZE*CUBE_ELEMENT_SIZE) + (gen.iy) * CUBE_ELEMENT_SIZE + (gen.jx) ];
